c3eecc3530d3bae2cb5a94b99e70a77de95ce546
a2926z <55917291+a2926z@users.noreply.github.com>
2023-02-24 18:25:17 +0000

    update contact form

--- contact.js
+++ contact.js
@@ -5,51 +5,11 @@ $(document).ready(function() {
       $("#popup").fadeIn(300);
     });
   
-    // Close popup
-    $("#close-popup").click(function() {
-      $("#popup").fadeOut(300);
-    });
-  
-    // Submit form
-    $("#submit").click(function(e) {
-      e.preventDefault();
-  
-      // Get form data
-      var name = $("#name").val();
-      var email = $("#email").val();
-      var message = $("#message").val();
-  
-      // Validate form data
-      if (name === '' || email === '' || message === '') {
-        alert("Please fill out all fields.");
-        return;
+    $("#popup, .close").click(function(event) {
+      if ($(event.target).is("#popup, .close")) { // Check if click is outside of form or on close button
+        event.preventDefault();
+        $("#popup").fadeOut(300);
       }
-  
-      // Create form data object
-      var formData = {
-        name: name,
-        email: email,
-        message: message
-      };
-  
-      // Send form data to server
-      $.ajax({
-        url: "send_email.php",
-        type: "POST",
-        data: formData,
-        success: function(response) {
-          alert("Your message was sent successfully!");
-          $("#name").val('');
-          $("#email").val('');
-          $("#message").val('');
-          $("#popup").fadeOut(300);
-        },
-        error: function(xhr, status, error) {
-          alert("There was an error sending your message. Please try again later.");
-          console.log(error);
-        }
-      });
-  
     });
-  
+
   });
